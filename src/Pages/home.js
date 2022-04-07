574ef7d02743d688b6e4b0f8155c885c0a3be2fa
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-07 11:53:54 +0530

    add: Home Page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import logo from "../logo.svg";
 
 export function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex max-w-full h-screen">
+      <div className="basis-1/2 bg-primaryCoral justify-center flex flex-col items-center">
+        <img src={logo} className="App-logo -mt-20" alt="logo" />
+        <h1 className=" font-bold text-6xl text-slate-900">WYZR</h1>
+        <h2 className=" font-bold text-3xl text-slate-800 mt-4">
+          Indian stories by Indian authors.
+        </h2>
+      </div>
+      <div className="basis-1/2 flex justify-center items-center">
+        <button className=" rounded-lg font-bold text-3xl p-4 bg-primaryCoral text-white">
+          Sign in with google
+        </button>
+      </div>
+    </div>
+  );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,3 @@
+export { Home } from "./home";
+export { Search } from "./search";
+export { BookDetails } from "./bookDetails";

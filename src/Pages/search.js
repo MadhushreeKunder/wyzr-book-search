3a34bc13e1f36ab0a15b4cb42c6ead4f25661f1d
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-08 23:40:44 +0530

    fix: styling

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -10,7 +10,6 @@ export function Search() {
     console.log(e.target.value);
     const book = e.target.value;
     setBook(book);
-    console.log(API_KEY);
   };
 
   const handleSubmit = (e) => {
@@ -21,31 +20,44 @@ export function Search() {
         "https://www.googleapis.com/books/v1/volumes?q=" +
           book +
           "&key=" +
-          API_KEY
+          API_KEY +
+          "&maxResults=20"
       )
       .then((data) => {
-        console.log(data.data.items);
+        console.log(data);
         setDisplay(data.data.items);
       });
   };
 
   return (
-    <div className="flex justify-center items-center h-screen max-w-screen-lg m-auto">
-      <div className="-mt-20">
-        Search
-        <form className=" " onSubmit={handleSubmit}>
+    <div className="flex justify-center max-w-screen-lg  m-auto mb-4">
+      <div className=" relative">
+        <form
+          className="sticky top-0 py-4 backdrop-blur bg-white/30 "
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             placeholder="Search your fav books.."
-            className="border-2 p-4"
+            className="border-2 p-4 backdrop-blur-lg bg-white/30 "
             onChange={handleBookSearchInput}
           />
-          <button type="submit">Search </button>
+          <button
+            type="submit"
+            className="ml-4 px-4 py-2 rounded-md text-white bg-primaryCoral font-medium uppercase"
+          >
+            Search{" "}
+          </button>
         </form>
-        <div className="flex">
+        <div className="flex flex-wrap justify-between ">
           {display.map((book) => (
-            <div key={book.id} className="border-2 border-black">
-              {book.volumeInfo.title}
+            <div key={book.id} className="border-2 w-44 p-2 my-4  text-left">
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                className="h-48"
+                alt={book.volumeInfo.title}
+              />
+              <p className="mt-2 font-medium">{book.volumeInfo.title}</p>
             </div>
           ))}
         </div>

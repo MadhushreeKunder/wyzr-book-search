0d616ecff7b1ef05d5a27cd30419fa2d902373d6
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-09 19:46:50 +0530

    refactor: async await

--- src/Pages/search.js
+++ src/Pages/search.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { API_KEY } from ".";
 
 export function Search() {
@@ -15,18 +16,24 @@ export function Search() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(book);
-    axios
-      .get(
-        "https://www.googleapis.com/books/v1/volumes?q=" +
-          book +
-          "&key=" +
-          API_KEY +
-          "&maxResults=20"
-      )
-      .then((data) => {
-        console.log(data);
-        setDisplay(data.data.items);
-      });
+    (async () => {
+      try {
+        await axios
+          .get(
+            "https://www.googleapis.com/books/v1/volumes?q=" +
+              book +
+              "&key=" +
+              API_KEY +
+              "&maxResults=20"
+          )
+          .then((data) => {
+            console.log(data);
+            setDisplay(data.data.items);
+          });
+      } catch (error) {
+        console.log("loading... error");
+      }
+    })();
   };
 
   return (
@@ -44,6 +51,7 @@ export function Search() {
           />
           <button
             type="submit"
+            onClick={handleSubmit}
             className="ml-4 px-4 py-2 rounded-md text-white bg-primaryCoral font-medium uppercase"
           >
             Search{" "}
@@ -51,14 +59,18 @@ export function Search() {
         </form>
         <div className="flex flex-wrap justify-between ">
           {display.map((book) => (
-            <div key={book.id} className="border-2 w-44 p-2 my-4  text-left">
-              <img
-                src={book.volumeInfo.imageLinks.thumbnail}
-                className="h-48"
-                alt={book.volumeInfo.title}
-              />
-              <p className="mt-2 font-medium">{book.volumeInfo.title}</p>
-            </div>
+            <Link to={`/book/${book.id}`} key={book.id}>
+              {/* <a href={book.volumeInfo.previewLink}> */}
+              <div className="border-2 w-44 p-2 my-4  text-left">
+                <img
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                  className="h-48"
+                  alt={book.volumeInfo.title}
+                />
+                <p className="mt-2 font-medium">{book.volumeInfo.title}</p>
+              </div>
+              {/* </a> */}
+            </Link>
           ))}
         </div>
       </div>

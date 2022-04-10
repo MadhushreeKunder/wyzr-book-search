0d4b94c115ffdc474fd465596f04d6f9a6358ffa
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-10 11:39:12 +0530

    try: get data from context

--- src/Pages/bookDetails.js
+++ src/Pages/bookDetails.js
@@ -7,35 +7,30 @@ export function BookDetails() {
   let { id } = useParams();
   const [showBookDetails, setShowBookDetails] = useState();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        await axios
-          .get(
-            "https://www.googleapis.com/books/v1/volumes/" +
-              id +
-              "?key=" +
-              API_KEY
-          )
-          .then((data) => {
-            console.log(data);
-            setShowBookDetails(data.data);
-            console.log(showBookDetails.volumeInfo.title);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-    // axios
-    //   .get(
-    //     "https://www.googleapis.com/books/v1/volumes/" + id + "?key=" + API_KEY
-    //   )
-    //   .then((data) => {
-    //     console.log(data);
-    //     setShowBookDetails(data.data);
-    //     console.log(showBookDetails.volumeInfo.title);
-    //   });
-  }, []);
+
+  // -------------useEffect to fetch data again (2nd api call)------------------
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       await axios
+  //         .get(
+  //           "https://www.googleapis.com/books/v1/volumes/" +
+  //             id +
+  //             "?key=" +
+  //             API_KEY
+  //         )
+  //         .then((data) => {
+  //           console.log(data);
+  //           setShowBookDetails(data.data);
+  //           console.log(showBookDetails.volumeInfo.title);
+  //         });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   })();
+
+  // }, []);
 
   // function getBookDetails(data, id) {
   //   return data.find((book) => book.id === id);

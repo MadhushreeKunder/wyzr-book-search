5443455e614c1712068f015a63ad294d73380ec5
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-11 23:59:13 +0530

    add: google sign in

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import GoogleAuth from "../Components/googleAuth";
+import GoogleAuthNew from "../Components/googleAuth";
+import { gapi } from "gapi-script";
+import Login from "../Components/googleAuth";
+
+const clientId =
+  "954832916377-thvj35j5mogu49ktj08cnc5a2oj8rado.apps.googleusercontent.com";
 
 export function Home() {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    };
+
+    gapi.load("client:auth2", start);
+  });
+
   return (
     <div className="flex flex-col md:flex-row max-w-full h-screen">
       <div className="basis-1/2 bg-primaryCoral justify-center flex flex-col items-center md:p-0 pt-10">
@@ -13,11 +31,14 @@ export function Home() {
         </h2>
       </div>
       <div className="basis-1/2 flex justify-center items-center">
-        <Link to="/search">
+        {/* <Link to="/search">
           <button className=" rounded-lg font-bold text-3xl bg-primaryCoral text-white md:m-0 md:p-4 p-2   mx-4">
-            Sign in with Google
+            Search books
           </button>
-        </Link>
+        </Link> */}
+        {/* <GoogleAuth /> */}
+        {/* <GoogleAuthNew/> */}
+        <Login/>
       </div>
     </div>
   );

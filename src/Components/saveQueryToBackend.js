0a0bc8afd2f85ddefbaa4af854d39325eb7719fc
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-12 16:49:59 +0530

    temp: save query to db

---
+++ src/Components/saveQueryToBackend.js
@@ -0,0 +1,31 @@
+// import { useData } from "../src/contexts/dataContext";
+
+// var mongo = require("mongodb");
+// const { MongoClient } = require("mongodb");
+
+// const uri = `mongodb+srv://MadhushreeKunder:mongoDB%237589@wyzr-cluster.44oxx.mongodb.net/test`;
+
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// export async function AddSearchQueryToDB() {
+//   const { book } = useData();
+
+//   try {
+//     await client.connect();
+//     const database = client.db("user");
+//     const searchquery = database.collection("searchquery");
+
+//     const newSearchQuery = { email: "zenorace@gmail.com", query: book };
+//     const result = await searchquery.insertOne(newSearchQuery);
+
+//     console.log(result);
+//     console.log(book);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// AddSearchQueryToDB().catch(console.dir);

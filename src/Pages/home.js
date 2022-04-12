69ab17167b18c340ba148115082e45b76bbbbf83
MadhushreeKunder <madhushreekunder@gmail.com>
2022-04-12 10:01:47 +0530

    check: typo

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -25,7 +25,7 @@ export function Home() {
     <div className="flex flex-col md:flex-row max-w-full h-screen">
       <div className="basis-1/2 bg-primaryCoral justify-center flex flex-col items-center md:p-0 pt-10">
         <img src={logo} className="App-logo -mt-20" alt="logo" />
-        <h1 className=" font-bold text-6xl text-slate-900">WYZR</h1>
+        <h1 className=" font-bold text-6xl text-slate-900"> WiZR </h1>
         <h2 className=" font-bold text-3xl text-slate-800 mt-4">
           Indian stories by Indian authors.
         </h2>
